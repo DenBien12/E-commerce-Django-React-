22f0027803e04481fb6d78403040cd8d80a81c89
Dang Le Hai <haidangle678@gmail.com>
2024-12-25 19:55:25 +0700

    Update routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'  
+
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-            <HomeScreen/>
-            </Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
       </main>
-      <Footer/>
-    </div>
+      <Footer />
+    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,19 +2,21 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     
       <Navbar expand="lg" className="bg-dark navbar-dark" collapseOnSelect>
       <Container>
-        <Navbar.Brand href="/">Dang's Shop</Navbar.Brand>
+        <Nav.Link as={Link} to='/'>
+          <Navbar.Brand>Dang's Shop</Navbar.Brand>
+          </Nav.Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-            <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+            <Nav.Link as={Link} to='/cart'><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+            <Nav.Link as={Link} to='/login'><i className='fas fa-user'></i>Login</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
