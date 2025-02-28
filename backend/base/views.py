6b24a623be4b12850ad8b4365c967fa4a1c88aaa
Dang Le Hai <haidangle678@gmail.com>
2025-02-28 13:22:53 +0700

    Update register user function

--- backend/base/views.py
+++ backend/base/views.py
@@ -10,9 +10,12 @@ from .serializers import ProductSerializer, UserSerializer, UserSerializerWithTo
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from rest_framework import status
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
 # Create your views here.
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
@@ -28,24 +31,22 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        'api/products/',
-        'api/products/create/',
-
-        'api/products/upload/',
-
-        'api/products/<id>/reviews/',
-
-        'api/products/top/',
-        'api/products/<id>/',
-
-        'api/products/delete/<id>/',
-        'api/products/update/<id>/',
-    ]
-    return Response(routes)
+    try:
+        user = User.objects.create(
+            first_name = data['name'],
+            username = data['email'],
+            email = data['email'],
+            password = make_password(data['password'])
+        )
+        seerializer = UserSerializerWithToken(user, many=False)
+        return Response(seerializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
